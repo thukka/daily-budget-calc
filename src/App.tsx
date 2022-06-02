43f11bfdbc178a8c1683c97f6af3df022273ff04
Toni Hukka <berendin@gmail.com>
2022-06-02 09:25:03 +0300

    user sum is saved to localStorage and retrieved on page load

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { ReactComponent as GitHub_Logo } from "./assets/github.svg";
 import calcDailyBudget from "./utils/calcDailybudget";
@@ -7,12 +7,25 @@ function App() {
     const [sum, setSum] = useState<string>("0");
     const dailyBudget = calcDailyBudget(sum);
 
+    useEffect(() =>{
+        const checkForSum = window.localStorage.getItem("userSum");
+        if (checkForSum) {
+            setSum(checkForSum);
+            (document.getElementById("input-sum") as HTMLInputElement).value = checkForSum;
+        }
+    }, []);
+
+    const setSumToStorage = (sum: string) => {
+        window.localStorage.setItem("userSum", sum);
+        setSum(sum);
+    };
+
     return (
         <div className="app">
             <div className="daily-budget">
                 {dailyBudget} / day
             </div>
-            <input type="number" placeholder="€" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSum(e.target.value)} />
+            <input id="input-sum" type="number" placeholder="€" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSumToStorage(e.target.value)} />
             <br />
             <div className="intro-animated">
                 Calculate your daily budget!
